70693724eaa1e70b78c8f2df202e80f44f5d6ce5
tt-le <tienthanh.le@mail.utoronto.ca>
2021-09-24 17:17:29 -0400

    basic frontend calling dummy api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import ReactDOM from 'react-dom';
+import { useState } from 'react';
+import api from './api';
 
 function App() {
+  const [rows, updateRows] = useState(null);
+  const clickHandler = async () => {
+    updateRows((await api.get('/get')).data.table.map(entry =><tr>{entry}</tr>))
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <button onClick={clickHandler} > click me to see the table! </button>
+        <form onSubmit={(e)=>{api.post(`/create/${e.target.elements.name.value}`); console.log(e.target.elements.name.value);}}> 
+        <input type="text" name="name"/>
+        <input type="submit" value="submit"/>
+        </form>
+        <div className="table">
+          {rows && (<table>{rows}</table>) }
+        </div>
     </div>
   );
 }
---
+++ frontend/src/api/index.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+// var url;
+// if( process.env.NODE_ENV === 'development') {
+//   url = `http://localhost:8080/dummy/`;
+// }
+
+export default axios.create({
+    baseURL: "http://localhost:8080/dummy/",
+});
