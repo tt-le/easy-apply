611fba3b778254fdc91b051520223d8d372027c8
tt-le <tienthanh.le@mail.utoronto.ca>
2021-10-07 14:13:46 -0400

    folder structure and dependencies for the frontend

---
+++ frontend/src/components/Login/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Formik, Form, Field } from "formik";
+import { Button, Grid, Link, Typography } from "@material-ui/core";
+import { TextField } from "formik-material-ui";
+
+
+const initialValues = {
+    email: "",
+    password: "",
+  };
+
+const LoginForm = () => (
+    <div>
+        
+    </div>
+)
---
+++ frontend/src/pages/Login/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import { Formik, Form, Field } from "formik";
+import { Button, Grid, Link, Typography } from "@material-ui/core";
+import { TextField } from "formik-material-ui";
+
