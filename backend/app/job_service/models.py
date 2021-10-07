6ecdfed80cc1a74b56339f1c5a28183839ed1130
JuefeiLu <juefeilu@gmail.com>
2021-10-06 22:19:25 -0400

    added job table to database with basic elements of job object

--- backend/app/job_service/models.py
+++ backend/app/job_service/models.py
@@ -1,12 +1,12 @@
 # Import the database object (db) from the main application module
 # We will define this inside /app/__init__.py in the next sections.
 from app import db
-
+ 
 # Define a base model for other database tables to inherit
 class Base(db.Model):
-
+ 
     __abstract__  = True
-
+ 
     id            = db.Column(db.Integer, primary_key=True)
     date_created  = db.Column(db.DateTime,  default=db.func.current_timestamp())
     date_modified = db.Column(db.DateTime,  default=db.func.current_timestamp(),
@@ -33,7 +33,7 @@ class Jobs(Base):
         self.industry       = industry 
         self.location       = location
         self.introduction   = introduction
-
-
+ 
+ 
     def __repr__(self):
-        return '<Jobs %r>' % (self.name)     
+        return '<Jobs %r>' % (self.name) 
