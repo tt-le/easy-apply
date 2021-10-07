5b2a9b7a30a91f31f72f9f4ab68aeb1274bcaaaf
tt-le <tienthanh.le@mail.utoronto.ca>
2021-10-07 19:18:34 -0400

    adding react route dom

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,16 +4,22 @@ import './index.css';
 import App from './App';
 import store from './app/store'
 import { Provider } from 'react-redux'
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
+import {BrowserRouter as Router} from "react-router-dom";
 
 ReactDOM.render(
+  <React.StrictMode>
   <Provider store={store}>
-    <App />
-  </Provider>,
+    <Router>
+      <App />
+    </Router>
+  </Provider>
+</React.StrictMode>
+,
   document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
