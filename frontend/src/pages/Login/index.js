be43fc0587572df8d069377eb06cf45ad569c5d4
Parth Solanki <parth@Parths-MacBook-Pro.local>
2021-10-08 13:43:59 -0400

    updated login page to have new background

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -7,25 +7,27 @@ import './index.css';
 
 const Login = () => (
     <body>
-    <div class="login">
-      <h1><img src={logo} alt="logo" style={ {"width":"100%"} }/></h1>
-      <Formik
-        initialValues={{
-          Email: '',
-          Password: '',
-        }}
-        onSubmit={async (values) => {
-          await new Promise((r) => setTimeout(r, 500));
-          alert(JSON.stringify(values, null, 2));
-        }}
-      >
-        <Form>
-          <input type="text" name="u" placeholder="Email" required="required" />
-          <input type="password" name="p" placeholder="Password" required="required" />
-        < button type="submit" class="btn btn-primary btn-block btn-large">Let me in.</button>
-        </Form>
-      </Formik>
-    </div>
+      <div class="body">
+        <div class="login">
+          <h1><img src={logo} alt="logo" style={ {"width":"100%"} }/></h1>
+          <Formik
+            initialValues={{
+              Email: '',
+              Password: '',
+            }}
+            onSubmit={async (values) => {
+              await new Promise((r) => setTimeout(r, 500));
+              alert(JSON.stringify(values, null, 2));
+            }}
+          >
+            <Form>
+              <input type="text" name="u" placeholder="Email" required="required" />
+              <input type="password" name="p" placeholder="Password" required="required" />
+            < button type="submit" class="btn btn-primary btn-block btn-large">Let me in.</button>
+            </Form>
+          </Formik>
+        </div>
+      </div>
     </body>
   );
 
