535a39223b84877fa475f9ed7de67a7d9b28f8f9
VanshikaVirmani12 <vansvirm@gmail.com>
2021-10-08 01:43:40 -0400

    Linked Dashboard page to router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,11 +4,13 @@ import { Route, Switch, Redirect } from "react-router-dom";
 // import api from './api';
 
 import Dummy from './pages/Dummy'
+import Dashboard from './pages/Dashboard'
 
 function App() {
   return (
     <Switch>
         <Route path="/" exact component={Dummy} />
+        <Route path="/Dashboard" exact component={Dashboard} /> 
       </Switch>
   );
 }
