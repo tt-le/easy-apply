256477c7a9a69060530b45e7a54e49efb94d8330
ShawLue <69040079+ShawLue@users.noreply.github.com>
2021-10-07 20:49:01 -0400

    login page done

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -2,4 +2,35 @@ import React from "react";
 import { Formik, Form, Field } from "formik";
 import { Button, Grid, Link, Typography } from "@material-ui/core";
 import { TextField } from "formik-material-ui";
+import './index.css';
 
+import ReactDOM from 'react-dom';
+const Login = () => (
+    <body>
+    <div>
+      <h1>Login</h1>
+      <Formik
+        initialValues={{
+          Email: '',
+          Password: '',
+        }}
+        onSubmit={async (values) => {
+          await new Promise((r) => setTimeout(r, 500));
+          alert(JSON.stringify(values, null, 2));
+        }}
+      >
+        <Form>
+          <label htmlFor="Email">Email</label>
+          <Field id="Email" name="Email" placeholder="Bob@gmail.com" />
+          
+          <label htmlFor="Password">Password</label>
+          <Field id="Password" name="Password" placeholder="yourpassword" />
+  
+          <button type="Login">Login</button>
+        </Form>
+      </Formik>
+    </div>
+    </body>
+  );
+
+  export default Login;
