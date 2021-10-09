4e86ef814e21935203393567eb45f1ba91b466d3
nooneuknow4 <81442581+nooneuknow4@users.noreply.github.com>
2021-10-08 20:43:22 -0400

    added CSS for the landing page

--- frontend/src/pages/Landing/index.js
+++ frontend/src/pages/Landing/index.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom';
 import Button from '@mui/material/Button'
+import './landing.css'
 
 function signup() {
     const landingPage = (
