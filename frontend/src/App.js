0e582124e6e375d5e65b4e9f430ad81040c75498
Tien Thanh Le <44335153+tt-le@users.noreply.github.com>
2021-10-08 22:14:50 -0400

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import { Route, Switch, Redirect } from "react-router-dom";
 // import api from './api';
 
 import Dummy from './pages/Dummy'
-import SignUp from './pages/SignUp'
+import SignUp from './pages/Signup'
 import Login from './pages/Login'
 
 function App() {
