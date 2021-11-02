b7469d9254943d64c590880c47655edf1703fe69
tt-le <tienthanh.le@mail.utoronto.ca>
2021-11-01 20:03:00 -0400

    TEAM-62 cors changes on api and axios

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -12,8 +12,8 @@ from flask_mail import Mail
 
 # Define the WSGI application object
 app = Flask(__name__)
-
-CORS(app)
+allowed_headers = ['Content-Type', 'Authorization', 'Access-Control-Allow-Headers','X-Requested-With','Access-Control-Allow-Origin']
+CORS(app,origins=['http://localhost:3000'], supports_credentials=True, allow_headers=allowed_headers)
 
 # Configurations
 app.config.from_object('config.DevelopmentConfig')
@@ -53,11 +53,11 @@ def require_role(role):
 def not_found(error):
     return render_template('404.html'), 404
 
-@app.after_request
-def apply_CORS(response):
-    response.headers["Access-Control-Allow-Origin"] = "http://localhost:3000"
-    response.headers["Access-Control-Allow-Headers"] =  "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With"
-    return response
+# @app.after_request
+# def apply_CORS(response):
+#     response.headers["Access-Control-Allow-Origin"] = "http://localhost:3000"
+#     response.headers["Access-Control-Allow-Headers"] =  "content-type, Access-Control-Allow-Headers, Authorization, X-Requested-With"
+#     return response
 
 # Import a module / component using its blueprint handler variable (mod_auth)
 from app.dummy_service.controllers import dummy_service 
--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -4,9 +4,12 @@ import axios from 'axios';
 // if( process.env.NODE_ENV === 'development') {
 //   url = `http://localhost:8080/dummy/`;
 // }
-
+// axios.defaults.withCredentials = true;
+// axios.defaults.crossorigin = true
 var req = axios.create({
     baseURL: "http://localhost:8080/",
+    withCredentials: true,
+    crossorigin: true
 });
 
 
