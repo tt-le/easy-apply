5567f309328d81bb7de19b449cd6a033c0b6a28d
Parth Solanki <parth@Parths-MacBook-Pro.local>
2021-11-25 10:19:21 -0500

    fixed the merge conflict issue

---
+++ frontend/src/Pages/Dashboard/index.js
@@ -0,0 +1,56 @@
+import {useState} from "react";
+import NavBar from "../../Components/NavBar"
+import api from "../../api";
+import Button from '@mui/material/Button';
+import data from "./mock_data.json"
+import "./dashboard.css";
+import {useHistory} from "react-router-dom"
+
+function DashBoard(){
+    const [tableInfo, setTableInfo] = useState(data);
+    const history = useHistory();
+
+    const handleClick = () => {
+        let path = "/postJob";
+        history.push(path);
+    }
+
+    return (
+        <div id="root">
+            <div id="navBar">
+                <NavBar/>
+            </div>
+            <div id ="dashboard_header">
+                <h2 id="dashboardBanner">Welcome, placeholder</h2>
+            </div>
+            <div id = "dashboard_body">
+                <h3 id="dashboardStatus">view your recent jobs</h3>
+                <table id = "table">
+                    <thead>
+                        <tr>
+                            <th>id</th>
+                            <th>position name</th>
+                            <th>company</th>
+                            <th>status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    {tableInfo.map((data) => (
+                        <tr>
+                        <td>{data.id}</td>
+                        <td>{data.position_name}</td>
+                        <td>{data.company}</td>
+                        <td><Button id = "button">view status</Button></td>
+                        </tr>
+                    ))}                   
+                    </tbody>
+                </table>
+            </div>
+            <div id="postJobButton">
+                <Button id = "button" onClick={handleClick}>post a new job</Button>
+            </div>
+        </div>
+    )
+};
+
+export default DashBoard
